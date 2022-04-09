c9f44aa9f8891fa1b7029b30d85ec37496f3c7a2
khalidfasely <faselykhalid01@gmail.com>
2022-04-09 14:17:15 +0000

    Finish adding styles for nav

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -13,20 +13,28 @@ export const Projects = ({ uname, projects }) => {
                         <div>
                         {
                             projects.map(project => (
-                                <div key={project.id}>
+                                <div className='projects__item' key={project.id}>
                                     <NavLink to={`/project/${project.id}`}>
-                                        <p>{project.name}</p>
-                                        <p>{project.time}</p>
+                                        <div className='projects__item-content'>
+                                            <p className='projects__item-content__name'>
+                                                <span className='mobile-screen'>{project.name.substring(0, 15)}...</span>
+                                                <span className='desktop-screen'>{project.name.substring(0, 25)}...</span>
+                                            </p>
+                                            <div className='projects__item-content__us-ti'>
+                                                <p className='projects__item-content__user'>{project.user.username}</p>
+                                                <p className='projects__item-content__time'>{project.time.substring(0, 11)}</p>
+                                            </div>
+                                        </div>
                                     </NavLink>
                                 </div>
                             ))
                         }
-                        <div>Create new project <Link to="/new-project">here</Link>.</div>
+                        <div className='projects__link-new'>Create new project <Link to="/new-project">here</Link>.</div>
                         </div> :
-                        <div>No projects. Create one <Link to="/new-project">here</Link>.</div>
+                        <div className='projects__link-new'>No projects. Create one <Link to="/new-project">here</Link>.</div>
                     }
                 </div> :
-                <div>
+                <div className='projects__anonymous'>
                     <Link to="/login">Login</Link> or <Link to="/register">Register</Link> to see your projects or create new ones!
                 </div>
             }
